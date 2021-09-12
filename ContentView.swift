2e3f07a495ff09592496271196265c0c52b57ed2
MattyAbby <88809174+MattyAbby@users.noreply.github.com>
2021-09-12 02:19:59 -0700

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,134 @@
+//
+//  ContentView.swift
+//  Counter App
+//
+//  Created by Matthew Abalos on 8/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isNight = false
+    
+    var body: some View {
+        NavigationView{
+            ZStack{
+                BackgroundView(isNight: $isNight)
+                NavigationLink(
+                    destination: Text("San Francisco, CA"),
+                    label: {
+                        Text("Random")
+                    })
+                VStack {
+                    cityTextView(cityName: "Cupertino, CA")
+                    
+                    MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: 76)
+                    
+                    HStack(spacing: 30){
+                        WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: "74")
+                        WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: "88")
+                        WeatherDayView(dayOfWeek: "THUR", imageName: "wind.snow", temperature: "69")
+                        WeatherDayView(dayOfWeek: "FRI", imageName: "sunset.fill", temperature: "60")
+                        WeatherDayView(dayOfWeek: "SAT", imageName: "snow", temperature: "55")
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        isNight.toggle()
+                    } label: {
+                        WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
+                    }
+                    
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: String
+    
+    var body: some View {
+        VStack{
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            Text(temperature)
+                .font(.system(size: 28, weight: .medium, design: .default))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+//    var topColor: Color
+//    var botColor: Color
+    
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue,
+                                                   isNight ? .gray : Color("lightBlue")]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct cityTextView: View {
+    
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+        .font(.system(size: 32, weight: .medium, design: .default))
+        .foregroundColor(.white)
+        .padding()
+    }
+}
+
+struct MainWeatherStatusView: View {
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View{
+        VStack(spacing: 10){
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            
+            Text("\(temperature)")
+                .font(.system(size:70, weight: .medium))
+                .foregroundColor(.white)
+        }
+        .padding(.bottom, 40)
+    }
+}
+
